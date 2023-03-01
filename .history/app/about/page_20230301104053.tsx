7cbcec3b47b97a5243b3f19ce0121a7d6be42e15
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-01 10:50:10 -0700

    Mobile update

--- .history/app/about/page_20230301104053.tsx
+++ .history/app/about/page_20230301104053.tsx
@@ -6,9 +6,9 @@ import BrandLogos from "ui/brand-icons";
 function About() {
   return (
     <div className="h-[78vh] max-h-full mx-auto px-8">
-      <div className="flex h-full md:items-center content-center overflow-y-auto overflow-x-clip z-30 mx-auto wi">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 max-w-md md:max-w-7xl md:p-4  md:items-center">
-          <div className="mt-8 md:mt-0">
+      <div className="flex h-full items-center content-center overflow-y-auto overflow-x-clip z-30 mx-auto wi">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 max-w-md md:max-w-7xl md:p-4  items-center">
+          <div className="mt-96 xs:mt-96 md:mt-0">
             <h1 className="text-xl font-extrabold uppercase">
               What we actually do.
             </h1>
--- .history/ui/Navigation/Foot_20230301003012.tsx
+++ .history/ui/Navigation/Foot_20230301003012.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { CardSocialIcons } from 'ui/social-icons'
 function Foot() {
   return (
-    <div className='flex max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl justify-between mx-auto pb-16 bottom-0 relative'>
+    <div className='flex max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl justify-between mx-auto pb-16 bottom-0 absolute'>
   <div className='font-bold text-white hover:font-extrabold'><Link href='https://app.gitbook.com/o/1eW19tluE2eCVR43yndk/s/Fr0SNse1kdKkE56c9c0z/'>DOCS</Link></div>
   <CardSocialIcons/>
   <div className='font-bold text-white hover:font-extrabold'><Link href='/contact'>CONTACT</Link></div>
