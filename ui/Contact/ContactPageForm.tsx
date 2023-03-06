67cb1c0d2fa4bf24d2b5c24dce762f52252da4cf
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-06 01:52:50 -0700

    Update ContactPageForm.tsx

--- ui/Contact/ContactPageForm.tsx
+++ ui/Contact/ContactPageForm.tsx
@@ -40,7 +40,7 @@ const ContactForm = (props: any) => {
     <div className="">
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white dark:text-white">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-white dark:text-gray-400 sm:text-xl">Need details about our pitch deck? Looking to collaborate? Anything. Let us know.</p>
+      <p className="mb-4 lg:mb-16 font-light text-center text-white dark:text-gray-400 sm:text-xl">Need details about our pitch deck? Looking to collaborate? Anything. Let us know.</p>
       <form onSubmit={handleSubmit} className="w-full">
       <div>
         <label htmlFor="email" className="block mb-2 text-sm font-medium text-white dark:text-gray-300">Your email</label>
@@ -53,14 +53,14 @@ const ContactForm = (props: any) => {
         />
         </div>
         <br />
-      <div>
-              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-white dark:text-gray-300">Your Name</label>
+      <div className='hidden md:block'>
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-white dark:text-gray-300">Your Name</label>
               <input className='shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-md focus:ring-red-300 focus:border-red-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-red-500 dark:focus:border-red-500 dark:shadow-sm-light required'
           type="text"
           name="name"
           value={formData.name}
           onChange={handleChange}
-          required
+        
         />
     </div>
       <br />
