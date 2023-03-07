8ea4fd407df71f9f9d396e713d86fb74219a4a98
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-07 03:02:39 -0700

    Update ProductItem.tsx

--- ui/Products/ProductItem.tsx
+++ ui/Products/ProductItem.tsx
@@ -6,7 +6,7 @@ function ProductItem({ item }: any) {
       <div className="bg-slate-900 -mt-8" key={item}>
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-4 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-6 gap-x-8 ">
-            <h1 className="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            <h1 className="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
               {item.title}
             </h1>
             {item.subtitle ?? <h2>{item.subtitle}</h2>}
