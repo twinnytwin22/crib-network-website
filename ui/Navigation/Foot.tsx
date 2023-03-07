d875b5e913a119d0ae8307546ac4a667fb372260
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-07 02:47:26 -0700

    ms

--- ui/Navigation/Foot.tsx
+++ ui/Navigation/Foot.tsx
@@ -5,7 +5,8 @@ function Foot() {
   return (
     <div className='flex max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl justify-between mx-auto pb-16 bottom-0 pt-2 relative'>
   <div className='font-bold text-white hover:font-extrabold'><Link href='https://app.gitbook.com/o/1eW19tluE2eCVR43yndk/s/Fr0SNse1kdKkE56c9c0z/'>DOCS</Link></div>
-  <CardSocialIcons/>
+  <div className='ml-8'>
+  <CardSocialIcons/></div>
   <div className='font-bold text-white hover:font-extrabold'><Link href='/contact'>CONTACT</Link></div>
     </div>
   )
--- ui/Navigation/Nav.tsx
+++ ui/Navigation/Nav.tsx
@@ -11,7 +11,7 @@ console.log(path)
     <div className='flex max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl justify-between mx-auto'>
      <div className='font-bold text-white hover:font-extrabold'><Link href='/about'>ABOUT</Link></div>
      {path !== '/' ?
-     <Link href='/'><img className='w-24' src='crib_W_.svg'></img></Link> : <div className='h-14'></div>}
+     <Link href='/'><img className='w-24 ml-8' src='crib_W_.svg'></img></Link> : <div className='h-14'></div>}
     <div className='font-bold text-white hover:font-extrabold'><Link href='/products'>PRODUCTS</Link></div>
     </div>
   )
